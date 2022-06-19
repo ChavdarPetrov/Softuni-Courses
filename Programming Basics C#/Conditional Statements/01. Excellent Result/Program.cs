5e8db0cac63cfcb5f5f6012f0789fcdbfdb92662
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-19 23:53:18 +0300

    Solutions

---
+++ Programming Basics C#/Conditional Statements/01. Excellent Result/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conditional_Statements___LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            
+        }
+    }
+}
---
+++ Programming Basics C#/Conditional Statements/07. Area of Figures/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{a * a:f3}");
+            }
+
+            else if (figure == "rectangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{b * c:f3}");
+
+            }
+
+            else if (figure == "circle")
+            {
+                double d = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{2*d * 2*d * Math.PI / 4:f3}");
+           
+            }
+        
+
+            else if (figure == "triangle")
+            {
+                double e = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{e * h / 2:f3}");
+
+            }
+        }
+
+}
+}
+
+
