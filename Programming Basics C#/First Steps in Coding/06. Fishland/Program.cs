fa88dbcf954565be80d732d640c2549bf01efbdf
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-19 23:41:38 +0300

    Solutions

---
+++ Programming Basics C#/First Steps in Coding/06. Fishland/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mackerelPrice = double.Parse(Console.ReadLine());
+            double spratPrice = double.Parse(Console.ReadLine());
+            double bonitoKg = double.Parse(Console.ReadLine());
+            double scadKg = double.Parse(Console.ReadLine());
+            double shrimpKg = double.Parse(Console.ReadLine());
+
+            double bonitoPrice = mackerelPrice * 1.6;
+            double scadPrice = spratPrice * 1.8;
+            double shrimpPrice = 7.5;
+
+            double totalPrice = bonitoPrice * bonitoKg + scadKg * scadPrice + shrimpKg * shrimpPrice;
+
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
