8cd589dfc2a7b2b360d2e649e49263cd1669dc25
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-20 22:06:48 +0300

    Solutions

---
+++ Programming Basics C#/Conditional Statements Advanced/11. Fruit Shop/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace _11.Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string item = Console.ReadLine();
+            string day = Console.ReadLine();
+            double amount = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+
+                     if  (item == "banana")
+                    {
+                        price = 2.5;
+                    }
+
+                    else if (item == "apple")
+                    {
+                        price = 1.2;
+                    }
+                    else if(item == "orange")
+                    {
+                        price = 0.85;
+                    }
+                    else if(item == "grapefruit")
+                    {
+                        price = 1.45;
+                    }
+
+                    else if(item == "kiwi")
+                    {
+                        price = 2.7;
+                    }
+
+                    else if(item == "pineapple")
+                    {
+                        price = 5.5;
+                    }
+
+                    else if(item == "grapes")
+                    {
+                        price = 3.85;
+                    }
+
+                    else
+                    {
+
+                        Console.WriteLine("error");
+
+                    }
+                    break;
+
+                case "Saturday":
+                case "Sunday":
+
+                    if (item == "banana")
+                    {
+                        price = 2.7;
+                    }
+
+                    else if(item == "apple")
+                    {
+                        price = 1.25;
+                    }
+                    else if(item == "orange")
+                    {
+                        price = 0.9;
+                    }
+                    else if(item == "grapefruit")
+                    {
+                        price = 1.6;
+                    }
+
+                    else if(item == "kiwi")
+                    {
+                        price = 3;
+                    }
+
+                    else if(item == "pineapple")
+                    {
+                        price = 5.6;
+                    }
+
+                    else if(item == "grapes")
+                    {
+                        price = 4.2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            double total = amount * price;
+
+            if (total != 0)
+            {
+                Console.WriteLine($"{total:f2}");
+
+            }
+        }
+    }
+}
---
+++ Programming Basics C#/Conditional Statements Advanced/12. Trade Commissions/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace _12.Trade_Commissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            double commission = 0;
+
+            switch (city)
+
+            {
+                case "Sofia":
+
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 0.05;
+                    }
+
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 0.07;
+                    }
+
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 0.08;
+                    }
+
+                    else if (sales > 10000)
+                    {
+                        commission = 0.12;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+
+                case "Varna":
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 0.045;
+                    }
+
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 0.075;
+                    }
+
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 0.1;
+                    }
+
+                    else if (sales > 10000)
+                    {
+                        commission = 0.13;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+
+                case "Plovdiv":
+
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 0.055;
+                    }
+
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 0.08;
+                    }
+
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 0.12;
+                    }
+
+                    else if (sales > 10000)
+                    {
+                        commission = 0.145;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+
+
+
+
+            }
+
+
+
+            double total = sales * commission;
+            if (city == "Sofia" || city == "Varna" || city == "Plovdiv")
+            {
+                if (sales >= 0)
+                {
+                    Console.WriteLine($"{total:f2}");
+
+                }
+
+            }
+        }
+    }
+}
