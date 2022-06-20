427cf070cb82fa99d6bd2293e4eaec4d21277c30
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-20 22:06:08 +0300

    Solutions

---
+++ Programming Basics C#/Conditional Statements Advanced/04. Personal Title/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.Personal_Title
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+
+            if (gender == 'f')
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+
+                }
+                else
+                { Console.WriteLine("Miss"); }
+            }
+
+            else if (gender == 'm')
+            {
+                if (age >= 16)
+                { Console.WriteLine("Mr."); }
+
+                else
+                { Console.WriteLine("Master"); }
+
+            }
+            
+            
+            
+            
+
+
+
+
+
+          
+        }
+    }
+}
