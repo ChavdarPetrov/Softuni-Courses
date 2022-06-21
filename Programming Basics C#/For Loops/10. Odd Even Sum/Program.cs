0e6026ed5145c70802be4a444cef6ce60d7c4e5c
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-21 23:08:39 +0300

    Solutions

---
+++ Programming Basics C#/For Loops/10. Odd Even Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _10.Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int nEven = int.Parse(Console.ReadLine());
+                    evenSum += nEven;
+                }
+                else
+                {
+                    int nOdd = int.Parse(Console.ReadLine());
+                    oddSum += nOdd;
+
+                }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.WriteLine("Yes");
+                    Console.WriteLine($"Sum = {evenSum}");
+                }
+                else
+                {
+
+                    Console.WriteLine("No");
+                    Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+            }
+        }
+    }
+}
