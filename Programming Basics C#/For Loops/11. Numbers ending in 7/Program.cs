eae66421ae773853aabce17567f13239432c0826
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-22 16:58:57 +0300

    Solutions

---
+++ Programming Basics C#/For Loops/11. Numbers ending in 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Programming Basics C#/For Loops/14. Clever Lilly/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04._Clever_Lilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            
+            double sum = 0;
+
+            for (int i = 1; i <=age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i * 5 - 1;
+                    
+                }
+
+                else
+                {
+                    sum += toyPrice;
+                }
+            }
+
+        
+
+            if (sum >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {sum-washingMachine:f2}");
+            }
+
+            else
+            {
+                Console.WriteLine($"No! {washingMachine-sum:f2}");
+            }
+
+
+
+
+        }
+    }
+}
