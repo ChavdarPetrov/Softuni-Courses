3990a827663d0b8af6afb9e72ccad983fc711bc6
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-22 16:59:42 +0300

    Solutions

---
+++ Programming Basics C#/For Loops/17. Trekking Mania/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupCount = int.Parse(Console.ReadLine());
+            double musala = 0;
+            double monblan = 0;
+            double kilimandjaro = 0;
+            double k2 = 0;
+            double everest = 0;
+
+
+            for (int i = 1; i <= groupCount; i++)
+            {
+                int peopleInGroup = int.Parse(Console.ReadLine());
+
+                if (peopleInGroup <= 5)
+                {
+                    musala+=peopleInGroup;
+                }
+               else if (peopleInGroup <= 12)
+                {
+                    monblan+=peopleInGroup;
+                }
+                else if (peopleInGroup <= 25)
+                {
+                    kilimandjaro+=peopleInGroup;
+                }
+                else if (peopleInGroup <= 40)
+                {
+                    k2+=peopleInGroup;
+                }
+                else
+                {
+                    everest+=peopleInGroup;
+            
+                }
+
+                
+
+
+            }
+
+
+            double sum = musala + monblan + kilimandjaro + k2 + everest;
+
+            Console.WriteLine($"{musala / sum * 100:f2}%");
+            Console.WriteLine($"{monblan / sum * 100:f2}%");
+            Console.WriteLine($"{kilimandjaro / sum * 100:f2}%");
+            Console.WriteLine($"{k2 / sum * 100:f2}%");
+            Console.WriteLine($"{everest / sum * 100:f2}%");
+
+
+
+        }
+    }
+}
