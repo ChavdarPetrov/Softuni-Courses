05a6651f8bcc178549f2c5b1c9f24f9a61894b79
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-25 13:03:17 +0300

    Solutions

---
+++ Programming Basics C#/While Loops/04. Account Balance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string amount = Console.ReadLine();
+            double total = 0;
+            while (amount != "NoMoreMoney")
+            {
+
+                double increase = double.Parse(amount);
+                if (increase < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {increase:f2}");
+                total += increase;
+                amount = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {total:f2}");
+
+        }
+    }
+}
---
+++ Programming Basics C#/While Loops/05. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace While_Loop___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (sum < n)
+            {
+                int a = int.Parse(Console.ReadLine());
+                sum += a;
+
+            }
+
+
+            Console.WriteLine(sum);
+        }
+    }
+}
