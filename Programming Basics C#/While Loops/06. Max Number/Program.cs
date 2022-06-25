ce0d90043306a8bc476a2aad632748eef02fd8b5
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-25 13:03:33 +0300

    Solutions

---
+++ Programming Basics C#/While Loops/06. Max Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int max = int.MinValue;
+            while (a != "Stop")
+            {
+                int b = int.Parse(a);
+                if (b > max)
+                {
+                    max=b;
+                }
+
+                a = Console.ReadLine();
+
+            }
+                Console.WriteLine(max);
+        }
+    }
+    }
