c3b89859880e3c9a181b337460998107a3b1bdee
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-25 13:03:54 +0300

    Solutions

---
+++ Programming Basics C#/While Loops/07. Min Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int min = int.MaxValue;
+            while (a != "Stop")
+            {
+                int b = int.Parse(a);
+                if (b < min)
+                {
+                    min = b;
+                }
+
+                a = Console.ReadLine();
+
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ Programming Basics C#/While Loops/08. Graduation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 0;
+
+            int grade = 1;
+            double total = 0;
+
+            while (grade <= 12)
+            {
+                double mark = double.Parse(Console.ReadLine());
+
+
+
+                if (mark < 4)
+                {
+                    counter++;
+                    if (counter == 2)
+                    {
+
+                        break;
+
+
+                    }
+                    continue;
+
+
+                }
+                    total += mark;
+                    grade++;
+
+            }
+                if (grade > 12)
+                {
+
+                Console.WriteLine($"{name} graduated. Average grade: {total/12:f2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} has been excluded at {grade} grade");
+                
+            }
+        }
+    }
+}
