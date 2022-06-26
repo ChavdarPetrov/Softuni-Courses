80ecd447641a5e2f8ab0aaff45bc8cf1aec0ce5b
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-26 13:54:30 +0300

    Solutions

---
+++ Programming Basics C#/While Loops/14. Cake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int pieces = a * b;
+            string take = "";
+            int counter = 0;
+
+            while (take != "STOP")
+            {
+                take = Console.ReadLine();
+
+                if (take == "STOP")
+                {
+                    Console.WriteLine($"{pieces - counter} pieces are left.");
+                    break;
+                }
+                counter += int.Parse(take);
+
+                if (counter > pieces)
+                {
+                    Console.WriteLine($"No more cake left! You need {counter - pieces} pieces more.");
+                    break;
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
