c498ceb963eff6966099d2db3f9ae216926effb1
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-27 15:39:09 +0300

    Solutions

---
+++ Programming Basics C#/05. Nested Loops/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Sum_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                        counter++;
+                    if (i + j == n)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {n})");
+                        return;
+                    }
+                   
+                    
+                        
+                    
+                }
+            }
+
+            
+            
+                Console.WriteLine($"{counter} combinations - neither equals {n}");
+
+            
+
+        }
+    }
+}
---
+++ Programming Basics C#/05. Nested Loops/05. Travelling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            double sum = 0;
+
+            while (destination != "End")
+            {
+            double budget = double.Parse(Console.ReadLine());
+                sum = 0;
+                while (sum < budget)
+                {
+                    sum += double.Parse(Console.ReadLine());
+                  
+                }
+
+                
+                    Console.WriteLine($"Going to {destination}!");
+
+                
+
+
+
+                destination = Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
