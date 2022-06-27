a9e7fcb885af7946416571911e952dc19639ddc7
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-27 15:52:51 +0300

    Solutions

---
+++ Programming Basics C#/05. Nested Loops/07. Number Pyramid/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentN =1;
+            bool isBigger = false;
+
+            for (int rows = 1; rows <=n ; rows++)
+            {
+                for (int i = 1; i <=rows; i++)
+                { 
+                  if (currentN >  n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(currentN + " ");
+                    currentN++;
+                }
+
+
+                if (isBigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
