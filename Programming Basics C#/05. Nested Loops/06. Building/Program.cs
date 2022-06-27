ec2b7c1519a9a2ea053ec53da22cece175fe9c40
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-27 15:39:28 +0300

    Solutions

---
+++ Programming Basics C#/05. Nested Loops/06. Building/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i > 0 ; i--)
+            {
+                for (int j = 0; j < b; j++)
+                { 
+                    if (i == a)
+                    {
+                        Console.Write($"L{i}{j} ");
+                        continue;
+
+                    }
+
+                     if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                }
+                        Console.WriteLine();
+
+            }
+
+
+
+
+
+
+                   }
+    }
+}
