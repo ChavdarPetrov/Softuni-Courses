cc853236520e5afb113e43c93349f5a3202df8ca
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-06-28 00:07:14 +0300

    Solutions

---
+++ Programming Basics C#/05. Nested Loops/08. Equal Sums Even Odd Positions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02._Equal_Sums_Even_Odd_Positions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstN = int.Parse(Console.ReadLine());
+            int secondN = int.Parse(Console.ReadLine());
+
+
+            for (int i = firstN; i <= secondN; i++)
+            {
+                string currentN = i.ToString();
+
+                int oddSum = 0;
+                int evenSum = 0;
+
+
+                for (int j = 0; j < currentN.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentN[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+
+                    }
+
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write(i + " ");
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Programming Basics C#/05. Nested Loops/10. Train the Trainers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Train_the_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string presentationName = Console.ReadLine();
+
+            double presentationEv = 0;
+            double totalPresentationEv = 0;
+            int totalPres = 0;
+            while (presentationName != "Finish")
+            {   
+                presentationEv = 0;
+                for (int i = 1; i <=n; i++)
+                {   
+                    double evaluation = double.Parse(Console.ReadLine());
+                    presentationEv += evaluation;
+                }
+                    presentationEv = presentationEv / n;
+
+                Console.WriteLine($"{presentationName} - {presentationEv:f2}.");
+
+                totalPresentationEv += presentationEv;
+
+                    totalPres++;
+
+                presentationName = Console.ReadLine();
+            }
+
+
+            Console.WriteLine($"Student's final assessment is {totalPresentationEv/totalPres :f2}. ");
+
+
+        }
+    }
+}
