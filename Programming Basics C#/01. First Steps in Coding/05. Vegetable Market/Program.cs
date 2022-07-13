a882bdf1ca38209ada6d24dfd0aca86bd79ec203
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-07-13 17:55:32 +0300

    Solutions

---
+++ Programming Basics C#/01. First Steps in Coding/05. Vegetable Market/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Vegetable_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            double vegetablesPerKg = double.Parse(Console.ReadLine());
+            double fruitsPerKg = double.Parse(Console.ReadLine());
+            int vegetablesKg = int.Parse(Console.ReadLine());
+            int fruitsKg = int.Parse(Console.ReadLine());
+
+            double sum = (vegetablesPerKg * vegetablesKg + fruitsPerKg * fruitsKg) / 1.94;
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
