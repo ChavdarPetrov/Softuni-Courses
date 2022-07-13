7a560c00f90e6c6f5b62ca20e80b32f69833d9a0
ChavdarPetrov <107720136+ChavdarPetrov@users.noreply.github.com>
2022-07-13 17:53:26 +0300

    Solutions

---
+++ Programming Basics C#/01. First Steps in Coding/10. Weather Forecast 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._Weather_Forecast_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
